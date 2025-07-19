e1dd55625e6ef423b38393d9af3dd6c4b6651100
pzhdoubler <phdoubler01@gmail.com>
2025-07-19 11:26:41 -0400

    finished dmet count func

--- seasonal_counts.py
+++ seasonal_counts.py
@@ -166,91 +166,87 @@ def count_DMET_on_DMET_percentiles(PERCENTILE_FOLDER, FREQ_FOLDER, INTENSITY_FOL
     cur_season = season[parse_precip_DMET_filename(all_files[0]).month]
 
     init = True
+    # calc runtime
+    start = time.time()
     for f, file in enumerate(all_files):
         dt = parse_precip_DMET_filename(file)
+        year = dt.year
         print(f"Counting frequencies for {dt.year} ...")
 
         data = netCDF4.Dataset(file,mode='r')
         times = data.variables["time"]
         times = np.ma.masked_invalid(times)
 
-        try:
-            # calc runtime
-            start = time.time()
+        for i, time in enumerate(times):
+            try:
+                dt = ref_time + datetime.timedelta(days=time)
+                # save current frequencies if switched to a new season
+                if season[dt.month] != cur_season:
+                    for p, percentile in enumerate(percentiles):
+                        percentile_str = percentile_title + str(percentile).replace(".","-")
+                        # frequency path
+                        f_name = f"{year}_{season_labels[cur_season]}_freq.csv"
+                        f_path = f"./{FREQ_FOLDER}/{percentile_str}/"
+                        if not os.path.exists(f_path):
+                            os.makedirs(f_path)
+                        try:
+                            save_NETCDF(lon2d, lat2d, frequency_count[:,:,p], 
+                                "EX-PR-FRQ", 
+                                "exceedences/season", 
+                                f"Top {percentile_str} Extreme Precip Freq", 
+                                f_path, f_name, 
+                                f"{season_labels[cur_season]} Freq of Top {percentile_str} Percentile Extreme Precipitation"
+                            )
+                        except Exception as e:
+                            print(f"Error saving {f_name}")
+                            print(e)
 
-            # save current frequencies if switched to a new season
-            if season[dt.month] != cur_season:
-                for p, percentile in enumerate(percentiles):
-                    percentile_str = percentile_title + str(percentile).replace(".","-")
-                    # frequency path
-                    f_name = f"{year}_{season_labels[cur_season]}_freq.csv"
-                    f_path = f"./{FREQ_FOLDER}/{percentile_str}/"
-                    if not os.path.exists(f_path):
-                        os.makedirs(f_path)
-                    try:
-                        save_NETCDF(lon2d, lat2d, frequency_count[:,:,p], 
-                            "EX-PR-FRQ", 
-                            "exceedences/season", 
-                            f"Top {percentile_str} Extreme Precip Freq", 
-                            f_path, f_name, 
-                            f"{season_labels[cur_season]} Freq of Top {percentile_str} Percentile Extreme Precipitation"
-                        )
-                    except Exception as e:
-                        print(f"Error saving {f_name}")
-                        print(e)
+                        # intensity path
+                        i_name = f"{year}_{season_labels[cur_season]}_avg-intensity.csv"
+                        i_path = f"./{INTENSITY_FOLDER}/{percentile_str}/"
+                        if not os.path.exists(i_path):
+                            os.makedirs(i_path)
+                        try:
+                            frequency_count[:,:,p] += frequency_count[:,:,p] == 0    # remove 0 valued counts to avoid div by 0
+                            intens_data = intensity_sum[:,:,p] / frequency_count[:,:,p]
+                            save_NETCDF(lon2d, lat2d, intens_data, 
+                                "EX-PR-INTENS", 
+                                "mm/day", 
+                                f"Top {percentile_str} Extreme Precip Average Intensity", 
+                                i_path, i_name, 
+                                f"{season_labels[cur_season]} Avg Intensity of Top {percentile_str} Percentile Extreme Precipitation"
+                            )
+                        except Exception as e:
+                            print(f"Error saving {i_name}")
+                            print(e)
 
-                    # intensity path
-                    i_name = f"{year}_{season_labels[cur_season]}_avg-intensity.csv"
-                    i_path = f"./{INTENSITY_FOLDER}/{percentile_str}/"
-                    if not os.path.exists(i_path):
-                        os.makedirs(i_path)
-                    try:
-                        frequency_count[:,:,p] += frequency_count[:,:,p] == 0    # remove 0 valued counts to avoid div by 0
-                        intens_data = intensity_sum[:,:,p] / frequency_count[:,:,p]
-                        save_NETCDF(lon2d, lat2d, intens_data, 
-                            "EX-PR-INTENS", 
-                            "mm/day", 
-                            f"Top {percentile_str} Extreme Precip Average Intensity", 
-                            i_path, i_name, 
-                            f"{season_labels[cur_season]} Avg Intensity of Top {percentile_str} Percentile Extreme Precipitation"
-                        )
-                    except Exception as e:
-                        print(f"Error saving {i_name}")
-                        print(e)
+                    # reset data
+                    frequency_count = np.zeros((lon2d.shape[0], lon2d.shape[1], len(percentiles)), dtype=np.int64)
+                    intensity_sum = np.zeros((lon2d.shape[0], lon2d.shape[1], len(percentiles)), dtype=np.float64)
+                    cur_season = season[dt.month]
 
-                # reset data
-                frequency_count = np.zeros((lon2d.shape[0], lon2d.shape[1], len(percentiles)), dtype=np.int64)
-                intensity_sum = np.zeros((lon2d.shape[0], lon2d.shape[1], len(percentiles)), dtype=np.float64)
-                cur_season = season[dt.month]
+                precip = data.variables["PRAVG"][i, :, :]
+                precip = np.ma.masked_invalid(precip)
 
-            # print progress
-            if f % (len(year_files) // 100) == 0:
-                print((f/len(year_files))*100, "% complete", end="\r")
+                # count frequency of each percentile and sum intensities of exceedences
+                for p in range(len(percentiles)):
+                    # do not count 0 events as exceedences
+                    zero_precip_grid = precip > 0.0 
+                    exceedence_grid = (precip >= percentile_grids[:,:,p,cur_season]) * zero_precip_grid 
+                    frequency_count[:,:,p] += exceedence_grid
+                    intensity_sum[:,:,p] += precip*exceedence_grid
 
-            data = netCDF4.Dataset(year_files[f],mode='r')
-
-            precip = data.variables["precipitation"][:][0][lat_slice[0]:lat_slice[1],lon_slice[0]:lon_slice[1]]
-            precip = np.ma.masked_where(precip <= -9999.0, precip)
-
-            # count frequency of each percentile and sum intensities of exceedences
-            for p in range(len(percentiles)):
-                # do not count 0 events as exceedences
-                zero_precip_grid = precip > 0.0 
-                exceedence_grid = (precip >= percentile_grids[:,:,p,cur_season]) * zero_precip_grid 
-                frequency_count[:,:,p] += exceedence_grid
-                intensity_sum[:,:,p] += precip*exceedence_grid
-
-            # print approximate runtime
-            if init:
-                end = time.time()
-                print(f"One file took {end - start} seconds")
-                print(f"All files estimated to take {(len(all_files) - f) * (end - start)} seconds")
-                init = False
-
-        except Exception as e:
-            print(f"Error with file {year_files[f]}")
-            print(e)
-            continue
+            except Exception as e:
+                print(f"Error with file {file}, time {time}")
+                print(e)
+                continue
+    
+        # print approximate runtime
+        if init:
+            end = time.time()
+            print(f"One file took {end - start} seconds")
+            print(f"All files estimated to take {(len(all_files) - f) * (end - start)} seconds")
+            init = False
 
 def count_CWRF_on_MSWEP_percentiles(PERCENTILE_FOLDER, FREQ_FOLDER, INTENSITY_FOLDER, all_files, percentiles):
     pass
